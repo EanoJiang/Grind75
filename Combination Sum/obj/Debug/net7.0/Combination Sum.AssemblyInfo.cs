755b4ff0fd7ea3ef93696fc048c785d3eb954e6d
Eano <934154737@qq.com>
2025-06-26 14:36:30 +0800

    西山居TA

--- Combination Sum/obj/Debug/net7.0/Combination Sum.AssemblyInfo.cs
+++ Combination Sum/obj/Debug/net7.0/Combination Sum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Combination Sum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff30539fc459fcfe9fe6a99fc1e34c2d0cd3cb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Combination Sum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Combination Sum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Valid Anagram/obj/Debug/net7.0/Valid Anagram.AssemblyInfo.cs
+++ Valid Anagram/obj/Debug/net7.0/Valid Anagram.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Valid Anagram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff30539fc459fcfe9fe6a99fc1e34c2d0cd3cb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Valid Anagram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Valid Anagram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
